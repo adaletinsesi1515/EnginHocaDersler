d3df2b97f28c288235176f4e41d8090cbeed6070
user user <48474@45663.onmicrosoft.com>
2021-01-29 14:17:17 +0300

    SQL Ödevi

---
+++ SQLQuery2.sql
@@ -0,0 +1,8 @@
+﻿SELECT Products.ProductName  UrunAdi, 
+SUM([Order Details].UnitPrice * [Order Details].Quantity) 
+ KazanilanToplamTutar
+FROM Products 
+INNER JOIN [Order Details] on Products.ProductID = [Order Details].ProductID 
+INNER JOIN Orders on Orders.OrderID = [Order Details].OrderID 
+GROUP BY Products.ProductName 
+ORDER BY ProductName, KazanilanToplamTutar
